18580e26ab06ee7f7e33f7b130aeb828a38e0e28
KartikLakhotiya <KARTIK.LAKHOTIYA064@nmims.in>
2024-05-23 16:44:43 +0530

    setup express server

--- server.ts
+++ server.ts
@@ -1,1 +1,13 @@
+import app from "./src/app"
+
 console.log("Welcome to Ebook APIs.")
+
+const StartServer = () => {
+    const port = process.env.PORT || 3000
+
+    app.listen(port, () => {
+        console.log(`Listening on https://localhost:${port}`)
+    })
+}
+
+StartServer();
