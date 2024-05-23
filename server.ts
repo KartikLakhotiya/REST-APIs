a9c6e3fc0e84477d9f60e8e5cc2cba91412cabc3
KartikLakhotiya <KARTIK.LAKHOTIYA064@nmims.in>
2024-05-23 16:56:10 +0530

    added config

--- server.ts
+++ server.ts
@@ -1,12 +1,13 @@
 import app from "./src/app"
+import { config } from "./src/config/config"
 
 console.log("Welcome to Ebook APIs.")
 
 const StartServer = () => {
-    const port = process.env.PORT || 3000
+    const port = config.port || 3000
 
     app.listen(port, () => {
-        console.log(`Listening on https://localhost:${port}`)
+        console.log(`Listening on Port ${port}`)
     })
 }
 
