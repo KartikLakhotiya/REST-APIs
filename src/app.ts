cae8eb370a76aae8dcfa273cc39c36d246509d7f
KartikLakhotiya <KARTIK.LAKHOTIYA064@nmims.in>
2024-05-25 00:24:27 +0530

    add book model

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { NextFunction, Request, Response } from 'express'
 import createHttpError, { HttpError } from 'http-errors';
 import { config } from './config/config';
 import userRouter from './user/userRouter';
+import bookRouter from './book/bookRouter';
 const app = express();
 
 app.use(express.json())
@@ -15,6 +16,7 @@ app.get('/', (req, res) => {
 });
 
 app.use("/api/users",userRouter);
+app.use("/api/books",bookRouter);
 
 // global error handler always comes at last.
 
