ce4f079ef3682d0c932283858d43d69cd1c25b15
KartikLakhotiya <KARTIK.LAKHOTIYA064@nmims.in>
2024-05-24 15:08:15 +0530

    Added user router

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,18 @@
 import express, { NextFunction, Request, Response } from 'express'
-import { HttpError } from 'http-errors';
+import createHttpError, { HttpError } from 'http-errors';
 import { config } from './config/config';
+import userRouter from './user/userRouter';
 const app = express();
 
 // Routes
 
 app.get('/', (req, res) => {
     res.json({message:"Welcome to my EBook Store."})
+    const error = createHttpError(400,"Something went wrong.")
+    throw error;
 });
+
+app.use("/api/users",userRouter);
 
 // global error handler always comes at last.
 
