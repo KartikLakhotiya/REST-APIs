583394662232ac28567778df81ba43e7df134124
KartikLakhotiya <KARTIK.LAKHOTIYA064@nmims.in>
2024-05-25 01:19:44 +0530

    upload files to cloudinary with config

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,9 +1,38 @@
 import { NextFunction, Request, Response } from "express";
+import cloudinary from "../config/cloudinary";
+import { Multer } from "multer";
+import path from "node:path";
 
 const createBook = async(req: Request, res: Response, next:NextFunction) => {
 
-    const {} = req.body;
-    res.json({message:"inside Book Controller"})
+    console.log('files', req.files);
+
+    const files = req.files as {[feildname: string]: Express.Multer.File[]}; // this line is useful only when you are using multer.
+
+    const coverImageMimeType = files.coverImage[0].mimetype.split('/').at(-1);
+    const fileName = files.coverImage[0].filename;
+    const filePath = path.resolve(__dirname,'../../public/data/uploads',fileName)
+
+    const uploadResult = await cloudinary.uploader.upload(filePath, {
+        filename_override: fileName,
+        folder: 'book-covers',
+        format: coverImageMimeType,
+    })
+
+    const bookFileName = files.file[0].filename;
+    const bookFilePath = path.resolve(__dirname,'../../public/data/uploads',bookFileName)
+
+    const bookFileUploadResult = await cloudinary.uploader.upload(bookFilePath,{
+        resource_type: 'raw',
+        filename_override: bookFileName,
+        folder: 'book-pdfs',
+        format: "pdf",
+    })
+
+    console.log('Book upload Result',bookFileUploadResult)
+    console.log('file upload result',uploadResult)
+    
+    res.json({message:"Form Data Received."})
 }
 
 export { createBook };
