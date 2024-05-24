28f6e4c23f69ed0ba95e9c7c78ec753c29bc4c45
KartikLakhotiya <KARTIK.LAKHOTIYA064@nmims.in>
2024-05-25 00:07:13 +0530

    add login user

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -33,7 +33,31 @@ const createUser = async(req: Request,res:Response,next:NextFunction) => {
     // Token Generation (JWT)
     const token = sign({sub: newUser._id},config.jwtSecret as string,{expiresIn:"7d", algorithm:"HS256"});
     
-    res.json({accessToken : token})
+    res.status(201).json({accessToken : token})
 }
 
-export { createUser };
+const loginUser = async(req: Request,res:Response,next:NextFunction) => {
+    const {email, password } = req.body;
+    if( !email || !password){
+        const error = createHttpError(400,"All Feilds are required.")
+        return next(error);
+    }
+
+    const user = await userModel.findOne({email})
+    if(!user){
+        return next(createHttpError(404,"User Not Found."))
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password as string);
+    if(!isMatch){
+        return next(createHttpError(400,"Username or Password Incorrect."))
+    }
+
+    //create access token.
+    const token = sign({sub: user._id},config.jwtSecret as string,{expiresIn:"7d", algorithm:"HS256"});
+    res.json({message:"User Logged in",accessToken: token})
+
+}
+
+
+export { createUser,loginUser };
